b6560b2c8159c63a38efb3244a5bab303044bbda
Dave Whiteland <dave@mysociety.org>
2017-01-18 12:13:37 +0000

    add scraper logic with SPARQL for getting Australian senator's areas
    code based on [Estonia area scraper](https://github.com/everypolitician-scrapers/estonia-constituencies/blob/master/scraper.rb)
    with acquisition of data commented out until we're getting the IDs back.
    
    Note: currently fails with error below
    
    ```
    .rbenv/versions/2.3.3/lib/ruby/gems/2.3.0/bundler/gems/wikidata-fetcher-97c64920301a/lib/wikidata.rb:49:in `block in sparql': undefined method `[]' for nil:NilClass (NoMethodError)
    	from /Users/beholder/.rbenv/versions/2.3.3/lib/ruby/gems/2.3.0/bundler/gems/wikidata-fetcher-97c64920301a/lib/wikidata.rb:49:in `map'
    	from /Users/beholder/.rbenv/versions/2.3.3/lib/ruby/gems/2.3.0/bundler/gems/wikidata-fetcher-97c64920301a/lib/wikidata.rb:49:in `sparql'
    	from scraper.rb:24:in `<main>'
    ```

---
+++ scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# encoding: utf-8
+# frozen_string_literal: true
+
+require 'pry'
+require 'scraperwiki'
+require 'wikidata/area'
+require 'wikidata/fetcher'
+
+# P39: position held
+# P768: electoral district
+
+query = <<QUERY
+  SELECT DISTINCT ?state
+  WHERE
+  {
+  	?item ps:P39 wd:%s .
+    ?item pq:P768 ?state .
+  }
+QUERY
+
+member_of_house = 'Q6814428' # member of the Australian Senate
+
+wanted = EveryPolitician::Wikidata.sparql(query % member_of_house)
+raise 'No ids' if wanted.empty?
+
+#data = Wikidata::Areas.new(ids: wanted).data
+#ScraperWiki.save_sqlite(%i(id), data)
